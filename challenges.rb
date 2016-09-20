debe7855fb3860071b26e990b28824bd0e0dfae5
Simon Wayne Pozder <pozdersw@dcsdk12.org>
2016-09-20 13:29:40 -0600

    Implement sum2

--- challenges.rb
+++ challenges.rb
@@ -1,10 +1,12 @@
 def everywhere?(list, value)
-  pair = 0
+  pair = 1
+  everywhere = true
 
   list.each do |n|
+
     if n == value
       pair = 1
-    elsif pair = 1
+    elsif pair == 1
       pair = 0
     else
       return false
@@ -12,4 +14,14 @@ def everywhere?(list, value)
   end
 
   true
+end
+
+def sum2(list)
+  if list.size >= 2
+    return list[0] + list[1]
+  elsif list.size == 1
+    return list[0]
+  end
+
+  return(0)
 end
--- challenges_test.rb
+++ challenges_test.rb
@@ -2,15 +2,39 @@ require 'minitest/autorun'
 require './challenges'
 
 describe 'challenges' do
-  it "is true when n is everywhere" do
-    everywhere?([1, 2, 1, 3], 1)
+
+  describe 'everywhere?' do
+
+    it "is true when n is everywhere" do
+      everywhere?([1, 2, 1, 3], 1).must_equal(true)
+    end
+
+    it "is false when number other than n is everywhere" do
+      everywhere?([1, 2, 1, 3], 2).must_equal(false)
+    end
+
+    it 'is false when n is not everywhere' do
+      everywhere?([1, 2, 1, 3, 4], 1).must_equal(false)
+    end
+
   end
 
-  it "is false when number other than n is everywhere" do
-    everywhere?([1, 2, 1, 3], 2)
+  describe 'sum2' do
+
+    it 'returns the sum of the first two' do
+      sum2([1, 2, 3]).must_equal(3)
+      sum2([1, 1, 1, 1]).must_equal(2)
+    end
+
+    it 'returns the sum of all numbers if there are only two in the array' do
+      sum2([1, 1]).must_equal(2)
+    end
+
+    it 'returns the sum of arrays with less than length 2' do
+      sum2([3]).must_equal(3)
+      sum2([]).must_equal(0)
+    end
+
   end
 
-  it 'is false when n is not everywhere' do
-    everywhere?([1, 2, 1, 3, 4], 1)
-  end
 end
